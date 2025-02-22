feb68ace732c36c664cd4823ac73c6fadbc21ad6
jorlar <jqern.larsen@gmail.com>
2025-02-22 17:16:55 +0100

    :art: Add dropdown for booking and calendar

--- src/components/KidsDropdown.js
+++ src/components/KidsDropdown.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useContext } from "react";
+//room Context
+import { RoomContext } from "../context/RoomContext";
+//Headless UI Menu
+import { Menu } from "@headlessui/react";
+//icons
+import { BsChevronDown } from "react-icons/bs";
+
+const lis = [
+  { name: "1 Barn" },
+  { name: "2 Barn" },
+  { name: "3 Barn" },
+  { name: "4 Barn" },
+];
 
 const KidsDropdown = () => {
-  return <div>KidsDropdown</div>;
+  return (
+    <Menu as='div' className='w-full h-full bg-white relative'>
+      {/* BTN */}
+      <Menu.Button className='w-full h-full flex items-center justify-between px-8'>
+        Barn
+        <BsChevronDown className='text-base text-accent-hover' />
+      </Menu.Button>
+      {/* Items */}
+      <Menu.Items
+        as='ul'
+        className='bg-white absolute w-full flex flex-col z-40'>
+        {lis.map((li, index) => {
+          return (
+            <Menu.Item
+              as='li'
+              className='border-b last-of-type:border-b-0 h-12 hover:bg-accent hover:text-white w-full flex justify-center items-center cursor-pointer'
+              key={index}>
+              {li.name}
+            </Menu.Item>
+          );
+        })}
+      </Menu.Items>
+    </Menu>
+  );
 };
 
 export default KidsDropdown;
