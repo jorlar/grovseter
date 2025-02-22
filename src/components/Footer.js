51e6a5f2a8f3c971459c9b90b5a7a4c654ea7793
jorlar <jqern.larsen@gmail.com>
2025-02-22 19:44:14 +0100

    :tada: Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+// logo
+import LogoWhite from "../assets/img/logo-white.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-primary py-12'>
+      <div className='container mx-auto text-white flex justify-between'>
+        {/*Logo*/}
+        <a href='/'>
+          <img src={LogoWhite} alt='' />
+        </a>
+        Copyright &copy; 2025, All rights reserved | Made by Jotun Utvikling AS
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
