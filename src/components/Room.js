69a4404d37f621774b79981581633470e71b789a
jorlar <jqern.larsen@gmail.com>
2025-02-23 20:22:05 +0100

    :art: Fix Link on room-card

--- src/components/Room.js
+++ src/components/Room.js
@@ -11,52 +11,54 @@ const Room = ({ room }) => {
   return (
     <div className='bg-white shadow-2xl min-h-[500px] group'>
       {/*IMG */}
-      <div className='overflow-hidden'>
-        <img
-          className='group-hover:scale-110 transition-all duration-300 w-full'
-          src={image}
-          alt=''
-        />
-      </div>
-      {/* details */}
-      <div className='bg-white shadow-lg max-w-[300px] mx-auto h-[60px] -translate-y-1/2 flex justify-center items-center uppercase font-tertiary tracking-[1px] font-semibold text-base'>
-        <div className='flex justify-between w-[80%]'>
-          {/*size*/}
-          <div className='flex items-center gap-x-2'>
-            <div className='text-accent'>
-              <BsArrowsFullscreen className='text-[15px]' />
+      <Link to={`/room/${id}`}>
+        <div className='overflow-hidden'>
+          <img
+            className='group-hover:scale-110 transition-all duration-300 w-full'
+            src={image}
+            alt=''
+          />
+        </div>
+        {/* details */}
+        <div className='bg-white shadow-lg max-w-[300px] mx-auto h-[60px] -translate-y-1/2 flex justify-center items-center uppercase font-tertiary tracking-[1px] font-semibold text-base'>
+          <div className='flex justify-between w-[80%]'>
+            {/*size*/}
+            <div className='flex items-center gap-x-2'>
+              <div className='text-accent'>
+                <BsArrowsFullscreen className='text-[15px]' />
+              </div>
+              <div className='flex gap-x-1'>
+                <div></div>
+                <div>{size}m2</div>
+              </div>
             </div>
-            <div className='flex gap-x-1'>
-              <div></div>
-              <div>{size}m2</div>
-            </div>
-          </div>
-          {/*Room Capacity*/}
-          <div className='flex items-center gap-x-2'>
-            <div className='text-accent'>
-              <BsPeople className='text-[18px]' />
-            </div>
-            <div className='flex gap-x-1'>
-              <div>{maxPerson}</div>
-              <div>Senger</div>
+            {/*Room Capacity*/}
+            <div className='flex items-center gap-x-2'>
+              <div className='text-accent'>
+                <BsPeople className='text-[18px]' />
+              </div>
+              <div className='flex gap-x-1'>
+                <div>{maxPerson}</div>
+                <div>Senger</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* Name & Description */}
-      <div className='text-center'>
-        <Link to={`/room/${id}`}>
+
+        {/* Name & Description */}
+        <div className='text-center'>
           <h3 className='h3'>{name}</h3>
+
+          <p className='max-w-[300px] mx-auto mb-3 lg:mb-6'>
+            {description.slice(0, 56)}
+          </p>
+        </div>
+        {/* button */}
+        <Link
+          to={`/room/${id}`}
+          className='btn btn-secondary btn-sm max-w-[240px] mx-auto'>
+          Book nå fra {price},-
         </Link>
-        <p className='max-w-[300px] mx-auto mb-3 lg:mb-6'>
-          {description.slice(0, 56)}
-        </p>
-      </div>
-      {/* button */}
-      <Link
-        to={`/room/${id}`}
-        className='btn btn-secondary btn-sm max-w-[240px] mx-auto'>
-        Book nå fra {price},-
       </Link>
     </div>
   );
