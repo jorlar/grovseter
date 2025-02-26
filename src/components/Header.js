18045cfcde07855852f30d7329f819a13cdc8b5e
jorlar <jqern.larsen@gmail.com>
2025-02-26 17:07:39 +0100

    :bug: Fix header. make sticky

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
     <header
       className={`${
         header ? "bg-white py-6 shadow-lg" : "bg-transparent py-8"
-      } fixed z-50 w-full transition-all duration-300`}>
+      }  z-50 w-full transition-all duration-300 sticky`}>
       <div className='container mx-auto flex flex-col items-center gap-y-6 lg:flex-row lg:justify-between lg:gap-y-0'>
         {/*logo*/}
         <a href='/'>
